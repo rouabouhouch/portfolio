f54b051a3b44066789a2003555cf5970cd433b10
Bouhouch <rouabouhouch777@gmail.com>
2024-03-25 00:35:52 +0100

    background fade

--- index.js
+++ index.js
@@ -10,3 +10,48 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+function startWave(element) {
+  const text = element.textContent;
+  const words = text.split(" ");
+  const fragment = document.createDocumentFragment();
+
+  words.forEach((word, wordIndex) => {
+    const wordContainer = document.createElement("span");
+
+    word.split("").forEach((char, charIndex) => {
+      const span = document.createElement("span");
+      span.textContent = char;
+      span.classList.add("letter");
+      span.style.animationDelay = `${
+        (wordIndex * word.length + charIndex) * 0.05
+      }s`; // Adjust delay as needed
+      wordContainer.appendChild(span);
+    });
+
+    fragment.appendChild(wordContainer);
+
+    if (wordIndex < words.length - 1) {
+      const spaceSpan = document.createElement("span");
+      spaceSpan.textContent = " ";
+      fragment.appendChild(spaceSpan);
+    }
+  });
+
+  element.innerHTML = "";
+  element.appendChild(fragment);
+}
+
+function stopWave(element) {
+  element.innerHTML = element.textContent;
+}
+window.addEventListener("scroll", function () {
+  var scrollPosition = window.scrollY;
+  var opacity = scrollPosition / window.innerHeight; // Adjust window.innerHeight for more/less intense fading
+  document.getElementById("hero").style.backgroundImage =
+    "linear-gradient(rgba(0, 0, 0, " +
+    opacity +
+    "), rgba(0, 0, 0, " +
+    opacity +
+    ')), url("hero.png")';
+});
